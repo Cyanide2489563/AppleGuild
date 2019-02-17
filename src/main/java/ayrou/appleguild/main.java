6cb9bbde6f8c099803c058e03d5cd1d7c3106b00
Cyanide <slm789456123@gmail.com>
2019-02-17 12:09:19 +0800

    init

---
+++ src/main/java/ayrou/appleguild/main.java
@@ -0,0 +1,16 @@
+package ayrou.appleguild;
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class main extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
