4de2505a0b06293b712ea10509343c531e72fb2c
cyanide2489563 <slm789456123@gmail.com>
2019-02-17 23:39:32 +0800

    新增主要監聽器

---
+++ src/main/java/com/Ayrou/AppleGuild/Listener/MainListener.java
@@ -0,0 +1,13 @@
+package com.Ayrou.AppleGuild.Listener;
+
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.player.PlayerLoginEvent;
+
+public class MainListener implements Listener {
+
+    @EventHandler
+    public void PlayerJoin(PlayerLoginEvent event) {
+        //TODO 玩家加入事件
+    }
+}
