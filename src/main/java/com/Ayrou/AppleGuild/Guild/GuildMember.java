e73df48a39988aa33e01c96eaa453db27cbd7170
BuildTools <unconfigured@null.spigotmc.org>
2019-02-18 20:44:04 +0800

    add GuildMember Class

---
+++ src/main/java/com/Ayrou/AppleGuild/Guild/GuildMember.java
@@ -0,0 +1,13 @@
+package com.Ayrou.AppleGuild.Guild;
+
+import java.util.UUID;
+
+public class GuildMember {
+    private UUID memberUUID;
+    private String memberName;
+    private int memberrole;
+}
+
+class Role {
+    
+}
