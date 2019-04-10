091a960a0b7550e7d88bb3829f7182a08a216fa3
Cyanide <slm789456123@gmail.com>
2019-04-10 20:37:06 +0800

    更新

---
+++ src/main/java/com/Ayrou/AppleGuild/GUI/Item/enums/ButtonType.java
@@ -0,0 +1,21 @@
+package com.Ayrou.AppleGuild.GUI.Item.enums;
+
+public enum ButtonType {
+    BLANK(1),
+    ARROW_UP(2),
+    ARROW_DOWN(3),
+    ARROW_LEFT(4),
+    ARROW_RIGHT(5),
+    LARROW_UP(6),
+    LARROW_DOWN(7),
+    LARROW_LEFT(8),
+    LARROW_RIGHT(9),
+    SORT_NUMBER(10),
+    SORT_NAME(11),
+    LIST(12),
+    OK(13);
+
+    ButtonType(int i) {
+
+    }
+}
