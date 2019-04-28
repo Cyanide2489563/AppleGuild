58fc4b0a00c6889f01bce91673e5d5615c15146a
cyanide2489563 <slm789456123@gmail.com>
2019-04-28 19:50:53 +0800

    更新

--- src/main/java/com/Ayrou/AppleGuild/GUI/Item/type/Button.java
+++ src/main/java/com/Ayrou/AppleGuild/GUI/Item/type/Button.java
@@ -1,13 +1,26 @@
 package com.Ayrou.AppleGuild.GUI.Item.type;
 
+import com.Ayrou.AppleGuild.GUI.Item.enums.ItemType;
+import org.bukkit.Material;
+import org.bukkit.inventory.ItemFlag;
 import org.bukkit.inventory.ItemStack;
+import org.bukkit.inventory.meta.ItemMeta;
 
-public class Button implements Item{
+import java.util.Objects;
 
+public class Button implements Item {
 
+    ItemType itemType = ItemType.BUTTON;
 
     @Override
     public ItemStack createItemStack() {
-        return null;
+        ItemStack itemStack = new ItemStack(Material.STONE_HOE);
+        ItemMeta itemmeta = itemStack.getItemMeta();
+
+        Objects.requireNonNull(itemmeta).setUnbreakable(true);
+        itemmeta.setDisplayName("");
+        itemmeta.addItemFlags(ItemFlag.HIDE_ATTRIBUTES, ItemFlag.HIDE_UNBREAKABLE);
+        itemStack.setItemMeta(itemmeta);
+        return itemStack;
     }
 }
--- src/main/java/com/Ayrou/AppleGuild/Listener/MainListener.java
+++ src/main/java/com/Ayrou/AppleGuild/Listener/MainListener.java
@@ -3,13 +3,14 @@ package com.Ayrou.AppleGuild.Listener;
 import com.Ayrou.AppleGuild.Guild.Guild;
 import com.Ayrou.AppleGuild.Guild.GuildManager;
 import com.Ayrou.AppleGuild.Main;
-import org.bukkit.Bukkit;
+import org.bukkit.Material;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerKickEvent;
 import org.bukkit.event.player.PlayerLoginEvent;
 import org.bukkit.event.player.PlayerQuitEvent;
+import org.bukkit.inventory.ItemStack;
 
 public class MainListener implements Listener {
 
